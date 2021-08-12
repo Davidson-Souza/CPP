9982ecfc7d97ba2e958a4f3342eb11c3a2cc764a
Davidson Souza <davidson.lucas.souza@outlook.com>
2021-08-12 03:38:34 +0300

    sha1_mining updated

--- src/cryptography/sha1_mining.cpp
+++ src/cryptography/sha1_mining.cpp
@@ -4,12 +4,16 @@
 #include <time.h>
 #include <vector>
 #include <math.h>
-
-
+/**
+ * This file is just a few tests on hashcash PoW, using SHA-1 (updating with SHA-2 is a todo, I guess)
+*/
+// I don't remember were this diff comes from, but a block's difficulty needs be lesser than this
+// Notice that it's just the 64 first bytes, since we don't use the lending
 #define MIN_DIFF 0x00000377ae000000
 
 #define ROTL(value, bits) (((value) << (bits)) | ((value) >> (32 - (bits))))
 #define ROTR(value, bits) (((value) >> (bits)) | ((value) << (32 - (bits))))
+
 #define T(i)\
             temp = (ROTL(a, 5) + f + e + k + w[i]);\
             e = d;  \
@@ -17,7 +21,15 @@
             c = ROTL(b, 30);\
             b = a;\
             a = temp;
-
+// This is my dummy block, note that this block is more like a token, it don't contains a reference
+// to the previous block, nor a set of txs
+typedef struct Block
+{
+    int height;
+    int nonce;
+    time_t timestamp;
+} block;
+// SHA-1 stuff, see the specs for details
 #define S(t) w[t] = ROTL(w[t-3] ^ w[t-8] ^ w[t-14] ^ w[t-16], 1);
 #define F1(i) f = (b & c) | ((~b) & d);k = 0x5A827999; T(i);
 #define F2(i) f = b ^ c ^ d; k = 0x6ED9EBA1; T(i);
@@ -30,19 +42,20 @@ unsigned int h2 = 0x98BADCFE;
 unsigned int h3 = 0x10325476;
 unsigned int h4 = 0xC3D2E1F0;
 unsigned int W[16] = {0x0};
-unsigned char blk1[] = {0x0};
 
 
 unsigned int a = h0, b = h1, c = h2, d = h3, e = h4, f = 0, temp = 0, k = 0, i = 0;
 unsigned int w[80];
 unsigned long target = MIN_DIFF;
 unsigned long hash;
-int mine(unsigned int *blk, char *dgst)
+
+Block& mine(unsigned int *blk, char *dgst)
 {
 while(1)
 {
     memcpy(w, blk, 16 * sizeof(int));
-
+    // unwrap booth message schedule and compression main loop, this avoids using costly operations
+    // like jmps and comparations
     S(16) S(17) S(18) S(19)
     S(20) S(21) S(22) S(23) S(24) S(25) S(26) S(27) S(28) S(29)
     S(30) S(31) S(32) S(33) S(34) S(35) S(36) S(37) S(38) S(39)
@@ -66,57 +79,68 @@ while(1)
     h3 = h3 + d;
     h4 = h4 + e;
     
+    // This script only verifyes to the fist 64 bits of the sha-1
     hash = ((unsigned long)h0 << 32) | h1;
-
+    
     if(hash <= target)
     {
         sprintf(dgst, "%08x%08x%08x%08x%08x", h0, h1, h2, h3, h4);
-        return blk[15];
+        return *((Block *) blk);
     }
-    blk[15] ++;
+    ((Block *)blk)->nonce++;    // is this memory safe? haha
 }
 
 }
 #define BLOCK_SPAN 60 * 10
 
-typedef struct Block
-{
-    int height;
-    time_t timestamp;
-} block;
 
+Block& createBlock(int height) 
+{
+    Block &blk = *(new Block);
+    blk.height = height;
+    blk.nonce = 0;
+    blk.timestamp = time(NULL);
+    return blk;
+}
 int main()
 {
-    int i = 0;
-    std::vector <block> blkList;
-    block *blk;
+    int epochFirst = time(NULL), height = 0;
+    auto blk = createBlock(height);
+    
+    // The size of the content is always this
+    W[15] = sizeof(Block);
 
-    W[15] = sizeof(time_t);
+    char dgst[32] = {0};
 
-    char dgst[50] = {0};
+    // Few nice info
     printf("Mining with %16lx diff\n", 0xFFFFFFFFFFFFFFFF/target);
 
 loop:
-    if((i % 2016) == 0 && i != 0) 
+    // Retarget after 2016 solutions, expected time between blocks is BLOCK_SPAN (10 min default).
+    if((height % 2016) == 0 && height != 0) 
     {
-        int actual = blkList.back().timestamp - blkList[i - 2016].timestamp;
+        int actual = blk.timestamp - epochFirst;
         target *= actual;
         target /= BLOCK_SPAN * 2016;
         if(target > MIN_DIFF)
         {
             target = MIN_DIFF;
         }
+        epochFirst = time(NULL);
         printf("Mining with %16lx diff - Timespan: %d - blk: %d - hashrate: %f h/s\n", MIN_DIFF/target, actual, i, (1/((float)target/0xFFFFFFFFFFFFFFFF))/BLOCK_SPAN);
     }
-    blk = new block;
-    blkList.push_back(*blk);
-    blkList.back().height = ++i;
-    blkList.back().timestamp = time(NULL);
+    // Copy the block data to the sha block, at this point, it's just a template and the nonce is 0
+    // our goal is find the correct nonce.
+    memcpy(W, &blk, sizeof(Block));
 
+    // Roll the nonces until find a proper solution
+    blk = mine(W, dgst);
+    // Show what we have found
+    printf("%04d %s %d\n", blk.height, dgst, blk.nonce);
+    height++;
+    blk = createBlock(height);
+goto loop;
 
-    W[0] = blkList.back().timestamp;
-    mine(W, dgst);
-    printf("%04d %s\n", blkList.back().height, dgst);
-    goto loop;
     return 0;
+
 }
